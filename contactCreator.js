9efde5c7f5f52b0e1c9f50cd2228e119fbeb1b99
jucezar <juliocezar.m.a@gmail.com>
2022-11-14 23:46:04 -0300

    Criando o arquivo para criação de um contato com os campo obrigatórios

--- contactCreator.js
+++ contactCreator.js
@@ -1,3 +1,33 @@
 import { LightningElement } from 'lwc';
 
-export default class ContactCreator extends LightningElement {}
+// importando modulo ShowToast - Mensagem de Sucesso
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+//importando objetos do Salesforce
+import  CONTACT_OBJECT  from '@salesforce/schema/Contact';
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+
+
+export default class ContactCreator extends LightningElement {
+
+objectApiName = CONTACT_OBJECT;
+fields = [ FIRSTNAME_FIELD, LASTNAME_FIELD, EMAIL_FIELD];
+
+ handlesuccess() {
+
+    const toastEvent = new ShowToastEvent({
+        title: 'Contato Criado',
+        message: 'ID do Registro: ' + event.detail.id,
+        variant: 'success'
+    });
+
+    this.dispatchEvent(toastEvent);
+ }
+
+ 
+
+
+
+}
